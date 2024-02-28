e8e0f046a6a5b165f3b992650e6b8b8c8c3410bd
ppvn <pop0vn3322@gmail.com>
2024-02-28 10:38:01 +0700

    добавлена функция комиссии

--- 1.py
+++ 1.py
@@ -15,14 +15,20 @@ def otkat(x):
 def tax(b):
     a = random.randint(1, 10)
     if a == 1:
-        business = 100000
+        business = 10000000
         age = random.randint(20, 40)
         income = business * age
-
+        if b - income > 0:
+            return b - income
+        else:
+            return b
+    else:
+        return b
 
 
 for state in range(50, 0, -1):
     b = fin(b, state)
     b = otkat(b)
+    b = tax(b)
     print(b)
 
