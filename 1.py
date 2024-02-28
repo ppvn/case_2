bbf3ec1344c08b90b96d529733877cb92e5c6c15
ppvn <pop0vn3322@gmail.com>
2024-02-28 10:33:02 +0700

    добавлена функция финансирования

--- 1.py
+++ 1.py
@@ -1,7 +1,11 @@
 b = 100000
 
-for i in range(50, 0, -1):
-    a = b / i
-    b = b - a
+
+def fin(x, i):
+    a = x / i
+    return x - a
+
+for state in range(50, 0, -1):
+    b = fin(b, state)
     print(b)
 
